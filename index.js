23982dc67c7dd2d0136fe95aa5cc0a950c65f994
Omar Jarvis <thejarvisgroupllc@gmail.com>
2021-04-26 02:46:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
 
-// Add your functions and code here
-function destructivelyAppendKitten(name){
+function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
 
@@ -9,11 +8,11 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
 
-function destructivelyRemoveLastKitten(name) {
-  kittens.pop(name)
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
 }
 
-function destructivelyRemoveFirstKitten(name) {
+function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
 
@@ -25,10 +24,10 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 
-function removeLastKitten() {
-  return kittens.pop()
+function removeFirstKitten() {
+  return kittens.slice(1)
 }
 
-function removeFirstKitten() {
-  return kittens.slice(0, kittens.length -1)
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
 }
