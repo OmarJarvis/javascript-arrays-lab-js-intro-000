2ee2331362a4d95bd5f2e7fdd86e7765f25d1b82
Omar Jarvis <thejarvisgroupllc@gmail.com>
2021-04-26 02:43:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,10 +25,11 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 
-function removeFirstKitten(name) {
+function removeLastKitten() {
+  return kittens.slice(1)
+}
+
+function removeFirstKitten() {
   return kittens.slice(0, kittens.length -1)
 }
 
-function removeLastKitten(name) {
-  return kittens.slice(1)
-}
